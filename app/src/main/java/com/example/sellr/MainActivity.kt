d1b99d5891c66862e990905dcf0d3b7710f7009d
ayush <ayush.gupta43.ag@gmail.com>
2022-12-21 21:45:43 +0530

    Bottom navigation bar

--- app/src/main/java/com/example/sellr/MainActivity.kt
+++ app/src/main/java/com/example/sellr/MainActivity.kt
@@ -2,10 +2,40 @@ package com.example.sellr
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.PopupMenu
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
+import androidx.navigation.fragment.findNavController
+import com.example.sellr.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainer)
+        val navController = navHostFragment!!.findNavController()
+
+        val popupMenu = PopupMenu(this, null)
+        popupMenu.inflate(R.menu.bottom_nav)
+        binding.bottomBar.setupWithNavController(popupMenu.menu, navController)
+
+        navController.addOnDestinationChangedListener(object : NavController.OnDestinationChangedListener{
+            override fun onDestinationChanged(
+                controller: NavController,
+                destination: NavDestination,
+                arguments: Bundle?
+            ) {
+                title = when(destination.id){
+                    R.id.cartFragment -> "Cart"
+                    R.id.profileFragment -> "Profile"
+                    else -> "Sellr"
+                }
+            }
+
+        })
+
     }
 }
