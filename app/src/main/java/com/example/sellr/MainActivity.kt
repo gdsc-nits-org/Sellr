8d67bc968ac2687cf4ba209571ec64e5ec7296d2
navneet098 <imnavneet1234@gmail.com>
2023-01-11 22:05:41 +0530

    Before Design

--- app/src/main/java/com/example/sellr/MainActivity.kt
+++ app/src/main/java/com/example/sellr/MainActivity.kt
@@ -23,30 +23,7 @@ class MainActivity : AppCompatActivity() {
         val fragement=ProfileFragment()
         fragmentTransaction.add(R.id.frame,fragement)
         fragmentTransaction.commit()
-//        binding = ActivityMainBinding.inflate(layoutInflater)
-//        setContentView(binding.root)
-//
-//        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainer)
-//        val navController = navHostFragment!!.findNavController()
-//
-//        val popupMenu = PopupMenu(this, null)
-//        popupMenu.inflate(R.menu.bottom_nav)
-//        binding.bottomBar.setupWithNavController(popupMenu.menu, navController)
-//
-//        navController.addOnDestinationChangedListener(object : NavController.OnDestinationChangedListener{
-//            override fun onDestinationChanged(
-//                controller: NavController,
-//                destination: NavDestination,
-//                arguments: Bundle?
-//            ) {
-//                title = when(destination.id){
-//                    R.id.cartFragment -> "Cart"
-//                    R.id.profileFragment -> "Profile"
-//                    else -> "Sellr"
-//                }
-//            }
-//
-//        })
+
 
     }
 }
--- app/src/main/java/com/example/sellr/data/UserData.kt
+++ app/src/main/java/com/example/sellr/data/UserData.kt
@@ -1,4 +1,6 @@
 package com.example.sellr.data
 
-data class UserData(val email:String="",val Hostel:String="",val name:String?="",val phonenum:String="",val scholarid:String?="",val uId:String="") {
+data class UserData(val email:String="",val Hostel:String="",
+                    val name:String?="",val phonenum:String="",
+                    val scholarid:String?="") {
 }
