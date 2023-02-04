3ba65abe928f41b754be45b73fe83826d4435561
Bibhas-programmed <110425865+Bibhas-programmed@users.noreply.github.com>
2023-02-04 16:29:25 +0530

    Initial commit 3

--- app/src/main/java/com/example/sellr/notification.kt
+++ app/src/main/java/com/example/sellr/notification.kt
@@ -1,14 +1,21 @@
 package com.example.sellr
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.app.PendingIntent
+import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.widget.RemoteViews
+import androidx.core.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
 
 class notification : FirebaseMessagingService() {
 
     val channelName = "notification channel"
-    val channelId = "com.example.pushnotificationpractice"
+    val channelId = "com.example.sellr"
 
     fun generateNotification(title: String, message: String) {
 
@@ -22,11 +29,43 @@ class notification : FirebaseMessagingService() {
 
 
         //getRemoteView function to pass RemoteViews
-        fun getRemoteView(title:String ,message:String ): RemoteViews {
-            val remoteView = RemoteViews("com.example.pushnotificationpractice", R.layout.notification)
+        fun getRemoteView(title:String ,message:String ): RemoteViews{
+            val remoteView = RemoteViews("com.example.sellr",R.layout.notificatioin_layout)
 
             remoteView.setTextViewText(R.id.name, title)
             remoteView.setTextViewText(R.id.message, message)
 
             return remoteView
         }
+
+
+        //creating Notification
+        var builder: NotificationCompat.Builder= NotificationCompat.Builder(applicationContext,channelId)
+            .setSmallIcon(R.drawable.notificationbell)
+            .setAutoCancel(true)
+            .setVibrate(longArrayOf(1000,1000,1000,1000))
+            .setOnlyAlertOnce(true)
+            .setContentIntent(pendingIntent)
+
+        //attaching Notification Layout to Notification
+        builder = builder.setContent(getRemoteView(title,message))
+
+        //notification Manager
+        val notificationManager= getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.O){
+            val notificationChannel = NotificationChannel(channelId,channelName,
+                NotificationManager.IMPORTANCE_DEFAULT)
+            notificationManager.createNotificationChannel(notificationChannel)
+        }
+        notificationManager.notify(0,builder.build())
+    }
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage){
+        if(remoteMessage.getNotification()!=null)
+        {
+            generateNotification(remoteMessage.notification!!.title!!, remoteMessage.notification!!.body!!)
+        }
+    }
+
+}
+
