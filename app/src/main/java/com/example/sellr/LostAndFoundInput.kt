9c6c66d60fffc8403e216c624583c18d02f64852
Shadow-of-sundered-star <prateek.mogha6@gmail.com>
2023-02-05 11:49:20 +0530

    Added layout and Adapter

--- app/src/main/java/com/example/sellr/LostAndFoundInput.kt
+++ app/src/main/java/com/example/sellr/LostAndFoundInput.kt
@@ -1,40 +1,112 @@
+@file:Suppress("DEPRECATION")
+
 package com.example.sellr
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+
+import android.view.View
+import android.widget.ImageView
+
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.example.sellr.data.LostAndFoundData
 import com.example.sellr.databinding.ActivityLostAndFoundInputBinding
-import com.google.firebase.database.DatabaseReference
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
+import java.util.*
 
 class LostAndFoundInput : AppCompatActivity() {
-    private lateinit var database : DatabaseReference
+    private lateinit var database : FirebaseDatabase
+    private lateinit var auth: FirebaseAuth
+    private lateinit var storage : FirebaseStorage
     private lateinit var binding : ActivityLostAndFoundInputBinding
+    private lateinit var selectedImg : Uri
+    private lateinit var dialog: AlertDialog.Builder
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLostAndFoundInputBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
+        dialog = AlertDialog.Builder(this)
+            .setMessage("Uploading Picture..")
+            .setCancelable(false)
+
+        database = FirebaseDatabase.getInstance("https://sellr-7a02b-default-rtdb.asia-southeast1.firebasedatabase.app/")
+        storage = FirebaseStorage.getInstance("gs://sellr-7a02b.appspot.com")
+        auth = FirebaseAuth.getInstance()
+
+
+        binding.lostandfoundImageButton.setOnClickListener {
+            val intent = Intent()
+            intent.action = Intent.ACTION_GET_CONTENT
+            intent.type = "image/*"
+            startActivityForResult(intent, 2)
+
+            //val intent = Intent(Intent.ACTION_PICK)
+            //intent.data = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+            //startActivityForResult(intent, 69)
+        }
+
         binding.lostandfoundInputfab.setOnClickListener {
 
-            val objectName = binding.lostandfoundObjectName.text.toString()
-            val objectLocation = binding.lostandfoundInputObjectLocation.text.toString()
-            val phoneNum = binding.lostandfoundInputUserContact.text.toString()
+            if(selectedImg == null){
+                findViewById<ImageView>(R.id.lostandfoundObjectimage).visibility = View.GONE
+            }
+            if(binding.lostandfoundObjectName.text!!.isEmpty()){
+                Toast.makeText(this,"Enter Object Name",Toast.LENGTH_SHORT).show()
+            }
+            else if(binding.lostandfoundInputObjectLocation.text!!.isEmpty()){
+                Toast.makeText(this,"Enter Object Location",Toast.LENGTH_SHORT).show()
+            }
+            else if(binding.lostandfoundInputUserContact.text!!.isEmpty()){
+                Toast.makeText(this,"Enter your contact ",Toast.LENGTH_SHORT).show()
+            }
+            else{
+                uploadData()
+            }
 
-            database = FirebaseDatabase.getInstance("https://sellr-7a02b-default-rtdb.asia-southeast1.firebasedatabase.app").getReference("LostAndFoundObjects")
+        }
+    }
 
-            val lostAndFoundObject = LostAndFoundData(objectName, objectLocation , phoneNum)
-            database.child(phoneNum).setValue(lostAndFoundObject).addOnSuccessListener {
-                binding.lostandfoundObjectName.text?.clear()
-                binding.lostandfoundInputObjectLocation.text?.clear()
-                binding.lostandfoundInputUserContact.text?.clear()
-
-                Toast.makeText(this,"Added Successfully",Toast.LENGTH_SHORT).show()
-            }.addOnFailureListener {
-                Toast.makeText(this,"Failed",Toast.LENGTH_SHORT).show()
+    private fun uploadData() {
+        val reference = storage.reference.child("LostAndFoundImages").child(Date().time.toString())
+        reference.putFile(selectedImg).addOnCompleteListener{
+            if(it.isSuccessful){
+                reference.downloadUrl.addOnSuccessListener { task ->
+                    uploadInfo(task.toString())
+                }
             }
         }
 
+    }
+
+    private fun uploadInfo(imgUrl: String) {
+        val lostAndFoundObject = LostAndFoundData(binding.lostandfoundObjectName.text.toString(),binding.lostandfoundInputObjectLocation.text.toString(),binding.lostandfoundInputUserContact.text.toString(),auth.uid.toString(),imgUrl )
+        database.reference.child("LostAndFound")
+            .child(auth.uid.toString())
+            .setValue(lostAndFoundObject)
+            .addOnSuccessListener {
+                Toast.makeText(this,"Object listed",Toast.LENGTH_SHORT).show()
+                startActivity(Intent(this,MainActivity::class.java))
+                finish()
+            }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (data != null ){
+            if(data.data != null){
+                selectedImg = data.data!!
+
+                //findViewById<ImageView>(R.id.lostandfoundObjectimage).setImageURI(selectedImg)
+            }
+        }
     }
 }
--- app/src/main/java/com/example/sellr/data/LostAndFoundData.kt
+++ app/src/main/java/com/example/sellr/data/LostAndFoundData.kt
@@ -3,7 +3,8 @@ package com.example.sellr.data
 data class LostAndFoundData(val objectName:String ?= null  ,
                             val objectLocation:String ?= null  ,
                             val contactNumber:String ?= null,
-                            val objectID:String ?= null)
+                            val UID:String ?= null,
+                            val imageUrl : String ?= null )
 {
 
 }
