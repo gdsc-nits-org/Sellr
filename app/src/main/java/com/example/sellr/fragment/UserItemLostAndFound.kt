62f935ec45a898b8d1be81986bec580f9fbb53ae
Dipan-17 <93267025+Dipan-17@users.noreply.github.com>
2023-02-09 00:08:03 +0530

    User lost nd found screen Sayad kaam kr jaae

--- app/src/main/java/com/example/sellr/fragment/UserItemLostAndFound.kt
+++ app/src/main/java/com/example/sellr/fragment/UserItemLostAndFound.kt
@@ -5,56 +5,62 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.sellr.R
+import androidx.recyclerview.widget.LinearLayoutManager
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import com.example.sellr.adapters.userItemLostFoundAdapter
+import com.example.sellr.data.LostAndFoundData
 
-/**
- * A simple [Fragment] subclass.
- * Use the [UserItemLostAndFound.newInstance] factory method to
- * create an instance of this fragment.
- */
+import com.example.sellr.databinding.FragmentUserItemLostAndFoundBinding
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.*
+import com.google.firebase.ktx.Firebase
+
+
 class UserItemLostAndFound : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+
+    val itemList = ArrayList<LostAndFoundData>()
+    lateinit var itemsAdapter: userItemLostFoundAdapter
+
+
+    private var viewBinding: FragmentUserItemLostAndFoundBinding? = null
+    private val binding get() = viewBinding!!
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user_item_lost_and_found, container, false)
+
+        viewBinding = FragmentUserItemLostAndFoundBinding.inflate(inflater, container, false)
+        retriveDataFromDatabase()
+        return binding.root
+
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment UserItemLostAndFound.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            UserItemLostAndFound().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+
+    private fun retriveDataFromDatabase() {
+        val database: FirebaseDatabase =
+            FirebaseDatabase.getInstance("https://sellr-7a02b-default-rtdb.asia-southeast1.firebasedatabase.app")
+        val myReference: DatabaseReference = database.reference.child("LostAndFound")
+        myReference.get().addOnSuccessListener {
+            val user = Firebase.auth.currentUser?.uid.toString()
+            itemList.clear()   //For clearing when data gets added to database.
+            for (eachItem in it.children) {
+                val item = eachItem.getValue(LostAndFoundData::class.java)
+                if (item != null && item.uid == user) {
+                    itemList.add(item)
+
                 }
+
+                itemsAdapter = userItemLostFoundAdapter(requireContext(), itemList)
+                binding.recyclerView.layoutManager = LinearLayoutManager(activity)
+                binding.recyclerView.adapter = itemsAdapter
             }
+
+        }.addOnFailureListener {
+
+        }
     }
+
 }
