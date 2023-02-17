c003419a8989b68c4027ecc49ccbe7a342f71907
Deep Saikia <96719584+saikiaDeep@users.noreply.github.com>
2023-02-17 20:41:58 +0530

    Image expands on click Updated on desc screen

--- app/src/main/java/com/example/sellr/DescriptionPage.kt
+++ app/src/main/java/com/example/sellr/DescriptionPage.kt
@@ -2,17 +2,23 @@ package com.example.sellr
 
 import android.animation.Animator
 import android.animation.AnimatorListenerAdapter
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
 import android.content.Intent
+import android.graphics.Point
+import android.graphics.Rect
+import android.graphics.RectF
 import android.net.Uri
 import android.os.Bundle
 import android.view.View
-import android.widget.LinearLayout
-import android.widget.Toast
+import android.view.animation.DecelerateInterpolator
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import com.denzcoskun.imageslider.ImageSlider
+import com.denzcoskun.imageslider.interfaces.ItemClickListener
 import com.denzcoskun.imageslider.models.SlideModel
 import com.example.sellr.databinding.ActivityDescrptionPageBinding
-import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.firebase.database.FirebaseDatabase
 import java.util.*
@@ -22,14 +28,16 @@ class DescriptionPage : AppCompatActivity() {
     private lateinit var binding: ActivityDescrptionPageBinding
     private lateinit var imageList: ArrayList<SlideModel>
     private lateinit var imageSlider: ImageSlider
+
     //fab idea
     //test
     private lateinit var backDrop: View
     private lateinit var lytMic: View
     private lateinit var lytCall: View
     private var rotate = false
-    private var emailSeller=""
-    private var phoneSeller=""
+    private var emailSeller = ""
+    private var phoneSeller = ""
+    private lateinit var expanded_image: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDescrptionPageBinding.inflate(layoutInflater)
@@ -44,7 +52,7 @@ class DescriptionPage : AppCompatActivity() {
             val value = extras.getString("key").toString()
             fetchDataFromDataBase(value)
         }
-
+        expanded_image = binding.expandedImage
         imageList = ArrayList()
         imageSlider = findViewById(R.id.image_slider)
 
@@ -98,7 +106,7 @@ class DescriptionPage : AppCompatActivity() {
 
     //For fab group display
     private fun toggleFabMode(v: View) {
-        rotate =  !rotate
+        rotate = !rotate
         if (rotate) {
             showIn(lytMic)
             showIn(lytCall)
@@ -149,17 +157,17 @@ class DescriptionPage : AppCompatActivity() {
     }
 
 
-
-
-    private fun fetchDataFromDataBase(items:String)
-    {
-        val databaseItem = FirebaseDatabase.getInstance("https://sellr-7a02b-default-rtdb.asia-southeast1.firebasedatabase.app").getReference("Items")
+    private fun fetchDataFromDataBase(items: String) {
+        val databaseItem =
+            FirebaseDatabase.getInstance("https://sellr-7a02b-default-rtdb.asia-southeast1.firebasedatabase.app")
+                .getReference("Items")
         databaseItem.child(items).get().addOnSuccessListener { dataSnapshot ->
 
             if (dataSnapshot.exists()) {
                 val primaryImage = dataSnapshot.child("imagePrimary").value.toString()
                 imageList.add(SlideModel(primaryImage))
                 val imageArray = ArrayList<String>()
+
                 imageArray.add(dataSnapshot.child("imageList").child("0").value.toString())
                 imageArray.add(dataSnapshot.child("imageList").child("1").value.toString())
                 imageArray.add(dataSnapshot.child("imageList").child("2").value.toString())
@@ -168,8 +176,13 @@ class DescriptionPage : AppCompatActivity() {
                         imageList.add(SlideModel(imageArray[i]))
                     }
                 }
-
+                val imageSlider = findViewById<ImageSlider>(R.id.image_slider)
                 imageSlider.setImageList(imageList)
+                imageSlider.setItemClickListener(object : ItemClickListener {
+                    override fun onItemSelected(position: Int) {
+                        expandImage(imageList[position].imageUrl.toString())
+                    }
+                })
                 var sold = dataSnapshot.child("sold").value.toString()
                 if (sold == "false") {
                     sold = "Available"
@@ -205,32 +218,197 @@ class DescriptionPage : AppCompatActivity() {
 
 
             }
-        }.addOnFailureListener{
+        }.addOnFailureListener {
             TODO("Not yet implemented")
         }
 
     }
+
 
     private fun fillUser(uid: Any?) {
-        val databaseUser = FirebaseDatabase.getInstance("https://sellr-7a02b-default-rtdb.asia-southeast1.firebasedatabase.app").getReference("Users")
+        val databaseUser =
+            FirebaseDatabase.getInstance("https://sellr-7a02b-default-rtdb.asia-southeast1.firebasedatabase.app")
+                .getReference("Users")
         databaseUser.child(uid.toString()).get().addOnSuccessListener { snapshot ->
 
-            if (snapshot.exists()){
+            if (snapshot.exists()) {
                 val email = snapshot.child("email").value.toString()
-                val phone=snapshot.child("phonenum").value.toString()
-                val name=snapshot.child("name").value.toString()
+                val phone = snapshot.child("phonenum").value.toString()
+                val name = snapshot.child("name").value.toString()
                     .replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString() }
-                binding.sellerName.text=name
-                binding.sellerEmail.text=email
-                binding.sellerPhone.text=phone
-                phoneSeller=phone
-                emailSeller=email
+                binding.sellerName.text = name
+                binding.sellerEmail.text = email
+                binding.sellerPhone.text = phone
+                phoneSeller = phone
+                emailSeller = email
             }
-        }.addOnFailureListener{
+        }.addOnFailureListener {
             TODO("Not yet implemented")
         }
     }
 
+
+    //WARNING
+    //ANYTHING BEYOND THIS COMMENT IS PURE BLACK MAGIC, DON'T CHANGE ANYTHING OR THE SPELL WILL BREAK
+    //COPY PASTED FROM THE DARK-HOLD OF ANDROID
+    private fun expandImage(imageUrl: String) {
+        val thumbView = binding.imageSlider
+        zoomImageFromThumb(thumbView, imageUrl)
+    }
+
+    // Hold a reference to the current animator
+    // so that it can be canceled midway.
+    private var currentAnimator: Animator? = null
+
+    // The system "short" animation time duration in milliseconds. This
+    // duration is ideal for subtle animations or animations that occur
+    // frequently.
+    private var shortAnimationDuration: Int = 400
+    private fun zoomImageFromThumb(thumbView: View, imageUrl: String) {
+        // If there's an animation in progress, cancel it
+        // immediately and proceed with this one.
+        currentAnimator?.cancel()
+
+        // Load the high-resolution "zoomed-in" image.
+        Glide.with(expanded_image).load(imageUrl).placeholder(R.drawable.loading_desc)
+            .into(expanded_image)
+        //expanded_image.setImageResource(imageResId)
+        binding.container.visibility = View.INVISIBLE
+        // Calculate the starting and ending bounds for the zoomed-in image.
+        val startBoundsInt = Rect()
+        val finalBoundsInt = Rect()
+        val globalOffset = Point()
+
+        // The start bounds are the global visible rectangle of the thumbnail,
+        // and the final bounds are the global visible rectangle of the container
+        // view. Set the container view's offset as the origin for the
+        // bounds, since that's the origin for the positioning animation
+        // properties (X, Y).
+        thumbView.getGlobalVisibleRect(startBoundsInt)
+        binding.container.getGlobalVisibleRect(finalBoundsInt, globalOffset)
+        startBoundsInt.offset(-globalOffset.x, -globalOffset.y)
+        finalBoundsInt.offset(-globalOffset.x, -globalOffset.y)
+        val startBounds = RectF(startBoundsInt)
+        val finalBounds = RectF(finalBoundsInt)
+
+        // Using the "center crop" technique, adjust the start bounds to be
+        // the same aspect ratio as the final bounds. This prevents
+        // undesirable stretching during the animation. Calculate the
+        // start scaling factor. The end scaling factor is always 1.0.
+        val startScale: Float
+        if ((finalBounds.width() / finalBounds.height() > startBounds.width() / startBounds.height())) {
+            // Extend start bounds horizontally.
+            startScale = startBounds.height() / finalBounds.height()
+            val startWidth: Float = startScale * finalBounds.width()
+            val deltaWidth: Float = (startWidth - startBounds.width()) / 2
+            startBounds.left -= deltaWidth.toInt()
+            startBounds.right += deltaWidth.toInt()
+        } else {
+            // Extend start bounds vertically.
+            startScale = startBounds.width() / finalBounds.width()
+            val startHeight: Float = startScale * finalBounds.height()
+            val deltaHeight: Float = (startHeight - startBounds.height()) / 2f
+            startBounds.top -= deltaHeight.toInt()
+            startBounds.bottom += deltaHeight.toInt()
+        }
+
+        // Hide the thumbnail and show the zoomed-in view. When the animation
+        // begins, it positions the zoomed-in view in the place of the
+        // thumbnail.
+        thumbView.alpha = 0f
+
+        animateZoomToLargeImage(startBounds, finalBounds, startScale)
+
+        setDismissLargeImageAnimation(thumbView, startBounds, startScale)
+    }
+
+    private fun animateZoomToLargeImage(startBounds: RectF, finalBounds: RectF, startScale: Float) {
+        expanded_image.visibility = View.VISIBLE
+
+        // Set the pivot point for SCALE_X and SCALE_Y transformations
+        // to the top-left corner of the zoomed-in view. The default
+        // is the center of the view.
+        expanded_image.pivotX = 0f
+        expanded_image.pivotY = 0f
+
+        // Construct and run the parallel animation of the four translation and
+        // scale properties: X, Y, SCALE_X, and SCALE_Y.
+        currentAnimator = AnimatorSet().apply {
+            play(
+                ObjectAnimator.ofFloat(
+                    expanded_image,
+                    View.X,
+                    startBounds.left,
+                    finalBounds.left
+                )
+            ).apply {
+                with(
+                    ObjectAnimator.ofFloat(
+                        expanded_image,
+                        View.Y,
+                        startBounds.top,
+                        finalBounds.top
+                    )
+                )
+                with(ObjectAnimator.ofFloat(expanded_image, View.SCALE_X, startScale, 1f))
+                with(ObjectAnimator.ofFloat(expanded_image, View.SCALE_Y, startScale, 1f))
+            }
+            duration = shortAnimationDuration.toLong()
+            interpolator = DecelerateInterpolator()
+            addListener(object : AnimatorListenerAdapter() {
+
+                override fun onAnimationEnd(animation: Animator) {
+                    currentAnimator = null
+                }
+
+                override fun onAnimationCancel(animation: Animator) {
+                    currentAnimator = null
+                }
+            })
+            start()
+        }
+    }
+
+    private fun setDismissLargeImageAnimation(
+        thumbView: View,
+        startBounds: RectF,
+        startScale: Float
+    ) {
+        // When the zoomed-in image is tapped, it zooms down to the
+        // original bounds and shows the thumbnail instead of
+        // the expanded image.
+        expanded_image.setOnClickListener {
+            currentAnimator?.cancel()
+
+            // Animate the four positioning and sizing properties in parallel,
+            // back to their original values.
+            currentAnimator = AnimatorSet().apply {
+                play(ObjectAnimator.ofFloat(expanded_image, View.X, startBounds.left)).apply {
+                    with(ObjectAnimator.ofFloat(expanded_image, View.Y, startBounds.top))
+                    with(ObjectAnimator.ofFloat(expanded_image, View.SCALE_X, startScale))
+                    with(ObjectAnimator.ofFloat(expanded_image, View.SCALE_Y, startScale))
+                }
+                duration = shortAnimationDuration.toLong()
+                interpolator = DecelerateInterpolator()
+                addListener(object : AnimatorListenerAdapter() {
+
+                    override fun onAnimationEnd(animation: Animator) {
+                        thumbView.alpha = 1f
+                        expanded_image.visibility = View.GONE
+                        currentAnimator = null
+                    }
+
+                    override fun onAnimationCancel(animation: Animator) {
+                        thumbView.alpha = 1f
+                        expanded_image.visibility = View.GONE
+                        currentAnimator = null
+                    }
+                })
+                start()
+            }
+            binding.container.visibility = View.VISIBLE
+        }
+    }
 
 
 }
