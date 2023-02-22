8146c044774e576ba77a1cf7907456f1f6f76324
Prateek <Prateek.mogha6@gmail.com>
2023-02-22 15:56:25 +0530

    -Multiple images for LostAndFound

--- app/src/main/java/com/example/sellr/adapters/userItemLostFoundAdapter.kt
+++ app/src/main/java/com/example/sellr/adapters/userItemLostFoundAdapter.kt
@@ -8,17 +8,11 @@ import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.example.sellr.DescriptionPage
 import com.example.sellr.LostAndFoundDescriptionPage
 import com.example.sellr.R
-import com.example.sellr.binding
 import com.example.sellr.data.LostAndFoundData
-import com.example.sellr.data.SellData
-import com.example.sellr.databinding.GridLayoutBinding
 import com.example.sellr.databinding.LostFoundItemCardBinding
-import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.*
-import com.google.firebase.ktx.Firebase
 
 
 class userItemLostFoundAdapter(
@@ -41,7 +35,7 @@ class userItemLostFoundAdapter(
 
     override fun onBindViewHolder(holder: userItemLostFoundAdapter.ItemsViewHolder, position: Int) {
         //holder.adapterBinding.itemImage.setImageResource(itemList[position].imagePrimary)
-        val url = itemList[position].imageUrl
+        val url = itemList[position].imagePrimary
         if (url=="NONE")
         {
             holder.adapterBinding.itemImage.setImageResource(R.drawable.no_image)
