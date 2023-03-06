0c95023d3269c1c37a6788702ac7288c00997b86
Deep Saikia <96719584+saikiaDeep@users.noreply.github.com>
2023-03-06 23:18:05 +0530

    Cart screen fixing initial commit

--- app/src/main/java/com/example/sellr/adapters/CartRVAdapter.kt
+++ app/src/main/java/com/example/sellr/adapters/CartRVAdapter.kt
@@ -1,5 +1,7 @@
 package com.example.sellr.adapters
+
 import android.content.Context
+import android.content.Intent
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
 import android.os.Build
@@ -12,18 +14,21 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.example.sellr.DescriptionPage
 import com.example.sellr.R
-import com.example.sellr.data.CartModel
+import com.example.sellr.data.SellData
+import com.example.sellr.databinding.CartLayoutBinding
+import com.example.sellr.databinding.GridViewBinding
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.ktx.Firebase
 import java.util.*
-import kotlin.collections.ArrayList
 
 private fun checkForInternet(context: Context?): Boolean {
 
     // register activity with the connectivity manager service
-    val connectivityManager = context?.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    val connectivityManager =
+        context?.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
 
     // if the android version is equal to M
     // or greater we need to use the
@@ -59,27 +64,30 @@ private fun checkForInternet(context: Context?): Boolean {
     }
 }
 
-class CartRVAdapter(private val context: Context?, cartModelArrayList: ArrayList<CartModel>) :
-    RecyclerView.Adapter<CartRVAdapter.ViewHolder>() {
-    var onItemClick: ((CartModel) -> Unit)? = null
-    private val cartModelArrayList: ArrayList<CartModel>
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+class CartRVAdapter(private val context: Context?,
+                    private var cartModelArrayList: ArrayList<SellData>) :
+    RecyclerView.Adapter<CartRVAdapter.ItemsViewHolder>() {
+
+   // private val cartModelArrayList: ArrayList<SellData>
+   inner class ItemsViewHolder(val adapterBinding: CartLayoutBinding) :
+       RecyclerView.ViewHolder(adapterBinding.root)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemsViewHolder {
         // to inflate the layout for each item of recycler view.
-        val view: View = LayoutInflater.from(parent.context).inflate(R.layout.cart_layout, parent, false)
-        return ViewHolder(view)
+       val binding=CartLayoutBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+
+        return ItemsViewHolder(binding)
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        // to set data to textview and imageview of each card layout
-        val model: CartModel = cartModelArrayList[position]
+    override fun onBindViewHolder(holder: ItemsViewHolder, position: Int) {
+        val model: SellData = cartModelArrayList[position]
 
-        var textRqrd=model.item_name
-        if(textRqrd.length >=11){
-            val dots="..."
-            textRqrd=textRqrd.substring(0,11)+dots
+        var textRqrd = model.productName
+        if (textRqrd!!.length >= 11) {
+            val dots = "..."
+            textRqrd = textRqrd.substring(0, 11) + dots
         }
 
-        holder.cartNameTV.text=textRqrd
+        holder.adapterBinding.itemName.text=textRqrd
             .replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString() }
 
 //        if(itemList[position].objectName!!.length >= 15){
@@ -93,54 +101,43 @@ class CartRVAdapter(private val context: Context?, cartModelArrayList: ArrayList
 //        }
 //
 
-        holder.cartPriceTV.text="Rs. ${model.item_price}"
-        Glide.with(holder.cartIV).load(model.item_image).centerCrop().into(holder.cartIV)
-        val btn=holder.removeButton
+        holder.adapterBinding.itemPrice.text = "Rs. ${model.price}"
+        Glide.with(holder.adapterBinding.itemImage).load(model.imagePrimary).centerCrop().into(holder.adapterBinding.itemImage)
+        val btn = holder.adapterBinding.removeButton
         btn.setOnClickListener {
-            if(checkForInternet(context)){
+            if (checkForInternet(context)) {
+                deleteModel(model.pid!!)
                 cartModelArrayList.removeAt(position)
                 notifyItemRemoved(position)
-                notifyItemRangeChanged(position, cartModelArrayList.size)
-                deleteModel(model.key)
-            }
-            else{
+                 notifyItemRangeChanged(position, cartModelArrayList.size)
+
+            } else {
                 Toast.makeText(context, "Connection failed", Toast.LENGTH_SHORT).show()
             }
         }
+        holder.itemView.setOnClickListener {
+            val value = cartModelArrayList[position].pid.toString()
+            val i = Intent(context, DescriptionPage::class.java)
+            i.putExtra("key", value)
+            context?.startActivity(i)
+        }
     }
+
     override fun getItemCount(): Int {
         return cartModelArrayList.size
     }
-    private fun deleteModel(model: String){
 
-        val database = FirebaseDatabase.getInstance("https://sellr-7a02b-default-rtdb.asia-southeast1.firebasedatabase.app").getReference("Users")
-        database.child(Firebase.auth.currentUser?.uid.toString()).child("favpost").child(model).removeValue().addOnSuccessListener {
-            Toast.makeText(context,"Item Removed From Cart",Toast.LENGTH_LONG).show()
-        }.addOnFailureListener{
-            Toast.makeText(context,"Error",Toast.LENGTH_LONG).show()
+    private fun deleteModel(model: String) {
+
+        val database =
+            FirebaseDatabase.getInstance("https://sellr-7a02b-default-rtdb.asia-southeast1.firebasedatabase.app")
+                .getReference("Users")
+        database.child(Firebase.auth.currentUser?.uid.toString()).child("favpost").child(model)
+            .removeValue().addOnSuccessListener {
+            Toast.makeText(context, "Item Removed From Cart", Toast.LENGTH_LONG).show()
+        }.addOnFailureListener {
+            Toast.makeText(context, "Error", Toast.LENGTH_LONG).show()
         }
     }
-    // View holder class for initializing of your views such as TextView and Imageview.
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val cartIV: ImageView
-        val cartNameTV: TextView
-        val cartPriceTV: TextView
-        val removeButton : ImageButton
-        init {
-            itemView.setOnClickListener {
-                onItemClick?.invoke(cartModelArrayList[adapterPosition])
-            }
-            cartIV = itemView.findViewById(R.id.item_image)
-            cartNameTV = itemView.findViewById(R.id.item_name)
-            cartPriceTV = itemView.findViewById(R.id.item_price)
-            removeButton=itemView.findViewById(R.id.remove_button)
 
-        }
-
-    }
-
-    // Constructor
-    init {
-        this.cartModelArrayList = cartModelArrayList
-    }
 }
--- app/src/main/java/com/example/sellr/fragment/CartFragment.kt
+++ app/src/main/java/com/example/sellr/fragment/CartFragment.kt
@@ -1,113 +1,152 @@
 package com.example.sellr.fragment
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.constraintlayout.widget.ConstraintLayout
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.example.sellr.DescriptionPage
-import com.example.sellr.R
 import com.example.sellr.adapters.CartRVAdapter
-import com.example.sellr.data.CartModel
+import com.example.sellr.data.SellData
+import com.example.sellr.databinding.FragmentCartBinding
 import com.google.firebase.auth.ktx.auth
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ValueEventListener
 import com.google.firebase.ktx.Firebase
 
 
 class CartFragment : Fragment() {
-    
+
+    lateinit var binding : FragmentCartBinding
+    private val cartModelArrayList=ArrayList<SellData>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        val view= inflater.inflate(R.layout.fragment_cart, container, false)
-        val cart = view.findViewById<RecyclerView>(R.id.idRVCourse)
-        val emptyIV=view.findViewById<ConstraintLayout>(R.id.emptyIV)
-        val cartModelArrayList: ArrayList<CartModel> = ArrayList()
-        val cartRVAdapter = CartRVAdapter(context, cartModelArrayList)
-        //name is linear but view is in grid
-        val linearLayoutManager = GridLayoutManager(context, 2)
-        cart.layoutManager = linearLayoutManager
-        cart.adapter = cartRVAdapter
+        binding = FragmentCartBinding.inflate(layoutInflater)
+        fetchDataFromDataBase()
+        return binding.root
+    }
+//    private fun retriveDataFromDatabase(){
+//        val database: FirebaseDatabase = FirebaseDatabase.getInstance("https://sellr-7a02b-default-rtdb.asia-southeast1.firebasedatabase.app")
+//        val myReference: DatabaseReference =database.reference.child("Items")
+//        myReference.get().addOnSuccessListener {
+//            val user=Firebase.auth.currentUser?.uid.toString()
+//            itemList.clear()   //For clearing when data gets added to database.
+//            for(eachItem in it.children){
+//                val item=eachItem.getValue(SellData::class.java)
+//                if(item!=null && item.userUID==user&&!item.sold!!){
+//                    itemList.add(item)
 
-        cartRVAdapter.onItemClick = { product ->
+    private fun fetchDataFromDataBase() {
+        binding.idRVCourse.adapter= context?.let {it1-> CartRVAdapter(it1,cartModelArrayList) }
+        binding.idRVCourse.layoutManager= GridLayoutManager(context,2)
+        println("Fetching data for cart screen")
+        val user = Firebase.auth.currentUser
+        val database= FirebaseDatabase.getInstance("https://sellr-7a02b-default-rtdb.asia-southeast1.firebasedatabase.app")
+        val myReference: DatabaseReference =database.reference.child("Users").child(
+            user?.uid.toString()).child("favpost")
+        myReference.get().addOnSuccessListener{
 
-            val value = product.itemID
-            val i = Intent(activity, DescriptionPage::class.java)
-            i.putExtra("key", value)
-            startActivity(i)
+            cartModelArrayList.clear()
+            //println(it.toString())
+            for(cartItemIDs in it.children){
+//                val item=cartItem.getValue(SellData::class.java)
+//                if(item!=null )
+//                {
+//                    if(!item.sold!!)
+//                    {
+//                        cartModelArrayList.add(item)
+//                    }
+//
+//                }
+                //get item ids to fetch
+                fetchIndividualItems(cartItemIDs.value.toString())
+                println(cartItemIDs.value.toString())
+            }
+
+        }.addOnFailureListener {
+
         }
 
-
-
-        val user = Firebase.auth.currentUser
-        val database= FirebaseDatabase.getInstance("https://sellr-7a02b-default-rtdb.asia-southeast1.firebasedatabase.app").getReference("Users").child(
-            user?.uid.toString()).child("favpost")
-        var count=0
-        database.addValueEventListener(object: ValueEventListener {
-            override fun onDataChange(snapshot: DataSnapshot) {
-
-                count++
-
-                if(snapshot.exists()){
-                    for(userSnapshot in snapshot.children){
-
-                        val items = userSnapshot.value.toString()
-
-                        val databaseItem = FirebaseDatabase.getInstance("https://sellr-7a02b-default-rtdb.asia-southeast1.firebasedatabase.app").getReference("Items")
-                        databaseItem.child(items).get().addOnSuccessListener {
-
-                            if (it.exists()){
-                                val sold=it.child("sold").value.toString()
-                                if(sold=="false" && count==1)
-                                {
-                                    val price = it.child("price").value
-                                    val name=it.child("productName").value
-                                    val image=it.child("imagePrimary").value
-                                    val key=userSnapshot.key.toString()
-                                    val id=it.child("pid").value.toString()
-                                    cartModelArrayList.add(CartModel(name.toString(),price.toString(),image.toString(),key,id))
-                                    cartRVAdapter.notifyItemInserted(cartModelArrayList.size-1)
-
-                                }
-
-                            }
-//                            if(cartModelArrayList.size==0)
-//                            {
-//                                emptyIV.visibility=View.VISIBLE
+//        database.addValueEventListener(object: ValueEventListener {
+//            override fun onDataChange(snapshot: DataSnapshot) {
+//                cartModelArrayList.clear()
+//                if(snapshot.exists()){
+//                    for(userSnapshot in snapshot.children){
+//
+//                        val items = userSnapshot.value.toString()
+//
+//                        val databaseItem = FirebaseDatabase.getInstance("https://sellr-7a02b-default-rtdb.asia-southeast1.firebasedatabase.app").getReference("Items")
+//                        databaseItem.child(items).get().addOnSuccessListener {
+//
+//                            if (it.exists()){
+//                                val sold=it.child("sold").value.toString()
+//                                if(sold=="false")
+//                                {
+//                                    val price = it.child("price").value
+//                                    val name=it.child("productName").value
+//                                    val image=it.child("imagePrimary").value
+//                                    val key=userSnapshot.key.toString()
+//                                    val id=it.child("pid").value.toString()
+//                                    cartModelArrayList.add(CartModel(name.toString(),price.toString(),image.toString(),key,id))
+//
+//                                }
+//
 //                            }
-//                            else
-//                            {
-//                                emptyIV.visibility=View.GONE
-//                            }
-                        }.addOnFailureListener{
-                            TODO("Not yet implemented")
-                        }
-
-                    }
-                }
-                else
-                    emptyIV.visibility=View.VISIBLE
-
-
-            }
-
-            override fun onCancelled(error: DatabaseError) {
-                TODO("Not yet implemented")
-            }
-
-        })
-        
-        return view
+////                            if(cartModelArrayList.size==0)
+////                            {
+////                                emptyIV.visibility=View.VISIBLE
+////                            }
+////                            else
+////                            {
+////                                emptyIV.visibility=View.GONE
+////                            }
+//                        }.addOnFailureListener{
+//                            TODO("Not yet implemented")
+//                        }
+//
+//                    }
+//                }
+//                else
+//                {
+//                    binding.emptyIV.visibility=View.VISIBLE
+//                }
+//                cartRVAdapter.notifyDataSetChanged()
+//
+//
+//
+//            }
+//
+//            override fun onCancelled(error: DatabaseError) {
+//                TODO("Not yet implemented")
+//            }
+//
+//        })
     }
-    
+
+    private fun fetchIndividualItems(itemID:String) {
+
+        val database: FirebaseDatabase = FirebaseDatabase.getInstance("https://sellr-7a02b-default-rtdb.asia-southeast1.firebasedatabase.app")
+        val myReference: DatabaseReference =database.reference.child("Items").child(itemID)
+        myReference.get().addOnSuccessListener {
+
+            val item=it.getValue(SellData::class.java)
+            if (item != null) {
+                if(item.sold != true){
+                    cartModelArrayList.add(item)
+                }
+            }
+            binding.idRVCourse.adapter?.notifyDataSetChanged()
+            binding.emptyIV.visibility = if (cartModelArrayList.isEmpty()) View.VISIBLE else View.GONE
+            binding.idRVCourse.visibility = if (cartModelArrayList.isEmpty()) View.GONE else View.VISIBLE
+
+        }.addOnFailureListener {
+            Toast.makeText(context, "Please try again!", Toast.LENGTH_SHORT).show()
+
+        }
+    }
+
 }
