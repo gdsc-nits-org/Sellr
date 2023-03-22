440a763643a29da24b2a37e1df18944ac2fd53de
Navneet Raj Karn <97835851+navneet098@users.noreply.github.com>
2023-03-22 11:37:36 +0530

    Add: Fragment No internet code added

--- app/src/main/java/com/example/sellr/noInternet.kt
+++ app/src/main/java/com/example/sellr/noInternet.kt
@@ -1,59 +1,54 @@
 package com.example.sellr
 
+import android.content.Context
+import android.content.Intent
+import android.net.ConnectivityManager
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.example.sellr.databinding.FragmentNoInternetBinding
+import com.example.sellr.databinding.FragmentProfileBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [noInternet.newInstance] factory method to
- * create an instance of this fragment.
- */
 class noInternet : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
+    private var viewBinding: FragmentNoInternetBinding?=null
+    private val binding get()= viewBinding!!
+
+    private var previousFragmentName: String? = null
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_no_internet, container, false)
+        viewBinding=FragmentNoInternetBinding.inflate(inflater,container,false)
+        val view=binding.root
+
+        arguments?.let {
+            previousFragmentName = it.getString("previousFragmentName")
+        }
+        binding.retrybutton.setOnClickListener {
+            if (isNetworkConnected(requireContext())){
+                previousFragmentName?.let {
+                    parentFragmentManager.popBackStack(it, 0)
+                }
+
+            }
+            else{
+                Toast.makeText(requireContext(), "Please check your internet and retry", Toast.LENGTH_LONG).show()
+            }
+        }
+
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment noInternet.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            noInternet().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    private fun isNetworkConnected(context: Context): Boolean {
+        val connectivityManager =
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val activeNetworkInfo = connectivityManager.activeNetworkInfo
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected
     }
+
 }
