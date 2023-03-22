17988b1dc5cca70b82ce240f31a5e07e40acd412
Navneet Raj Karn <97835851+navneet098@users.noreply.github.com>
2023-03-22 18:51:46 +0530

    Fix: For android 12

--- app/src/main/java/com/example/sellr/utils/NetworkChangeReceiver.kt
+++ app/src/main/java/com/example/sellr/utils/NetworkChangeReceiver.kt
@@ -2,6 +2,9 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.core.content.ContextCompat.startActivity
 import androidx.fragment.app.FragmentManager
 import androidx.navigation.fragment.findNavController
@@ -33,9 +36,15 @@ class NetworkChangeReceiver(private val fragmentManager: FragmentManager) : Broa
     }
 
 
+    @RequiresApi(Build.VERSION_CODES.M)
     private fun isNetworkConnected(context: Context?): Boolean {
-        val connectivityManager = context?.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        val networkInfo = connectivityManager.activeNetworkInfo
-        return networkInfo != null && networkInfo.isConnected
+        val connectivityManager =
+            context?.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val network = connectivityManager.activeNetwork
+        if (network != null) {
+            val networkCapabilities = connectivityManager.getNetworkCapabilities(network)
+            return networkCapabilities?.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET) ?: false
+        }
+        return false
     }
 }
